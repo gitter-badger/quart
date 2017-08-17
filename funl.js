189026f8a97102ee4c55c799cf65fbfd26ad7e6a
Schahriar SaffarShargh <info@schahriar.com>
2017-08-16 22:09:18 -0700

    Docs

--- funl.js
+++ funl.js
@@ -1,7 +1,15 @@
 const http2 = require('http2');
 const Router = require('./lib/Router');
 
+/**
+ * funl web server
+ */
 class FUNL {
+  /**
+   * Creates a new funl instance
+   * @param {object} [options] - Options passed to createSecureServer such as `cert` and `key`
+   * @param {Http2SecureServer} [instance] - Optional http2 instance to be used instead of creating a brand new one
+   */
   constructor(options, server) {
     // Store options
     this.options = options;
@@ -17,18 +25,36 @@ class FUNL {
     this.server.on('stream', (stream, headers) => this.router.routeStream(stream, headers));
   }
 
-  on(route, handler) {
-    this.router.on(route, handler);
+  /**
+   * Listen on a path
+   * @param {string} path - url/path to map handler to
+   * @param {function} handler - an *async* function that accepts a stream object as the first argument
+   */
+  on(path, handler) {
+    this.router.on(path, handler);
   }
 
+  /**
+   * Adds a middleware that always executes before listeners
+   * @param {function} handler - an *async* function that accepts a stream object as the first argument
+   */
   preflight(handler) {
     this.router.preflight(handler);
   }
 
+  /**
+   * Adds a postflight middleware that always executes after listeners
+   * @param {function} handler - an *async* function that accepts a stream object as the first argument
+   */
   postflight(handler) {
     this.router.postflight(handler);
   }
 
+  /**
+   * Bind instance to listen on a given port
+   * @param {number} port - an open port
+   * @param {function} callback
+   */
   listen(port, callback) {
     this.server.listen(port, callback);
   }
