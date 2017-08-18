89f6387f90ef789abadcbbb00864a65fae61fbfe
Schahriar SaffarShargh <info@schahriar.com>
2017-08-17 21:49:46 -0700

    Moved bash script to NPM pretest

--- test/init.js
+++ test/init.js
@@ -2,37 +2,19 @@ const fs = require('fs');
 const path = require('path');
 const { exec } = require('child_process');
 const Quart = require('../Quart');
-const make_certs_file = path.resolve(__dirname, './make-certs.sh');
-
-fs.chmodSync(make_certs_file, 777);
-
-const make_certs = exec(`. ${make_certs_file}`, {
-  cwd: path.resolve(__dirname, './certs')
-});
-
-make_certs.stdout.pipe(process.stdout);
-make_certs.stderr.pipe(process.stderr);
 
 // Require globals
 global.libraryPath = path.resolve(__dirname, '../lib/');
 global.requireFromLibrary = (file) => require(path.resolve(libraryPath, file));
 
-describe("Cert Generation", function () {
-  it("should generate certs", (done) => {
-    make_certs.on('close', () => {
-      const cert = fs.readFileSync(path.resolve(__dirname, './certs/all/my-private-root-ca.cert.pem'), 'utf8');
-      const key = fs.readFileSync(path.resolve(__dirname, './certs/all/my-private-root-ca.privkey.pem'), 'utf8');
+const cert = fs.readFileSync(path.resolve(__dirname, './certs/all/my-private-root-ca.cert.pem'), 'utf8');
+const key = fs.readFileSync(path.resolve(__dirname, './certs/all/my-private-root-ca.privkey.pem'), 'utf8');
 
-      // Assign test globals
-      global.expect = require('chai').expect;
-      global.app = new Quart({ key, cert });
-      global.cert = cert;
-      global.key = key;
-
-      done();
-    });
-  });
-});
+// Assign test globals
+global.expect = require('chai').expect;
+global.app = new Quart({ key, cert });
+global.cert = cert;
+global.key = key;
 
 // Unit tests
 require('./unit/MapList_unit.js');
